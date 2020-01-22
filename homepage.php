584acff3ebdb2cde0581d7bea4daf65c8410d7f3
amin foroutan <aminforoutan7@gmail.com>
2020-01-22 22:03:12 +0330

    adding the homepage template

---
+++ homepage.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: HomePage */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
