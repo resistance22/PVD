c23738d59954967375768b55749401843656477c
amin foroutan <aminforoutan7@gmail.com>
2020-01-30 15:46:37 +0330

    creating the footer markup

--- footer.php
+++ footer.php
@@ -16,10 +16,62 @@
 	<footer id="colophon" class="site-footer">
 		<div class="container">
 			<div class="row no-gutters">
-				<div class="col-sm-12 col-md-6 col-lg-3"></div>
-				<div class="col-sm-12 col-md-6 col-lg-3"></div>
-				<div class="col-sm-12 col-md-6 col-lg-3"></div>
-				<div class="col-sm-12 col-md-6 col-lg-3"></div>
+				<div class="col-sm-12 col-md-6 col-lg-3 footer-col">
+					<div class="footer-inside">
+						<div class="title">
+							<h4>
+								<?php _e('درباره‌ی ما','pvd'); ?>
+							</h4>
+						</div>
+						<div class=content>
+							<p>
+							لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 footer-col">
+					<div class="footer-inside">
+						<div class="title">
+							<h4>
+								<?php _e('آدرس','pvd'); ?>
+							</h4>
+						</div>
+						<div class=content>
+							<p>
+							لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+							</p>
+							<p>
+							لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+							</p>
+							<p>
+							لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 footer-col">
+					<div class="footer-inside">
+						<div class="title">
+							<h4>
+								<?php _e('تماس با ما','pvd'); ?>
+							</h4>
+						</div>
+						<div class=content>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 footer-col">
+					<div class="footer-inside">
+						<div class="title">
+							<h4>
+								<?php _e('نشان‌ها','pvd'); ?>
+							</h4>
+						</div>
+						<div class=content>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</footer><!-- #colophon -->
