b5ddf6d059cabd30fdf39f4a8705634ef4310916
amin foroutan <aminforoutan7@gmail.com>
2020-02-04 17:55:14 +0330

    adding the container elements in archive title container

--- custom-archive.php
+++ custom-archive.php
@@ -5,27 +5,22 @@ get_header();
 ?>
    <?php 
       if(have_posts()):
-         while(have_posts(  )):
+			while(have_posts(  )):
+				the_post();
    ?>
+	<div class="custom-archive-title-cont">
+		<div class="container">
+			<h1 class="custom-archive-title">
+				<?php the_title(); ?>
+			</h1>
+		</div>
+	</div>
+
+
 	<div class="entry-content">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pvd' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pvd' ),
-			'after'  => '</div>',
-		) );
 		?>
 	</div><!-- .entry-content -->
    <?php
