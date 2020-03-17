4620d11490582e9123484d9da21689c3c9f29816
amin <aminforoutan7@gmail.com>
2020-03-17 18:39:11 +0330

    adding the main.js file to the project on L 130

--- functions.php
+++ functions.php
@@ -127,6 +127,7 @@ function pvd_scripts() {
 	wp_enqueue_script( 'pvd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'pvd-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'main.js', get_template_directory_uri(). '/js/main.js', ['jquery'], null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -160,10 +161,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-
-
-
-
-
-
