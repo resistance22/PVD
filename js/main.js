f024f0883f272a85188b620093a9115adbddef54
amin <aminforoutan7@gmail.com>
2020-03-18 15:04:07 +0330

    adding the logic of third column of the dropdown menu

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ jQuery(document).ready(function($){
             $(this).removeClass('active');
         })
         $(elem).addClass('active');
+
+
         $('.sub-cat-cont').each(function(index){         
             $(this).css({
                 "display":"none",
@@ -20,13 +22,37 @@ jQuery(document).ready(function($){
 
     
     $(document).on('mouseenter','.product-sub-item',function(e){
-        let elem = e.target;
+        let elem = e.target,
+        cat = elem.getAttribute("self-cat");
+
         $('.product-sub-item').each(function(i){
             $(this).removeClass('active');
         })
         $(elem).addClass('active');
-        
+        $('.product-cat-cont').each(function(index){         
+            $(this).css({
+                "display":"none",
+            });
+            if( this.getAttribute('parent-sub-cat') == cat ){
+                $(this).css({
+                    "display":"block",
+                });
+            }
         })
+    })
+
+
+
+
+    $(document).on('mouseenter','.product-item',function(e){
+        let elem = e.target,
+        cat = elem.getAttribute("p-id");
+
+        $('.product-item').each(function(i){
+            $(this).removeClass('active');
+        })
+        $(elem).addClass('active');
+    })
 
     $(document).on('mouseenter','.product-menu',function(e){
         $('.op-menu').css({
@@ -42,4 +68,24 @@ jQuery(document).ready(function($){
         })
     })
 
+
+
+
+
+    var swiper = new Swiper('.homepage-swiper', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay:{
+            delay:4000,
+            disableOnInteraction:false
+        },
+        speed: 850,
+        loop:true,
+        fadeEffect: true
+    });
 })
