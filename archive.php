d84973780771cc2e643d0781d738395859c9e8f7
amin <aminforoutan7@gmail.com>
2020-03-19 15:10:43 +0330

    changing the layout of archive pages

--- archive.php
+++ archive.php
@@ -13,42 +13,45 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+		<div class="container">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				<header class="archive-page-header">
+					<?php
+						the_archive_title('<h1 class="archve-title">','</h1>');
+					?>
+				</header><!-- .page-header -->
+				<div class="row no-gutters">
+			<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content-arch', get_post_type() );
+
+				endwhile;
 				?>
-			</header><!-- .page-header -->
+			</div>
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				the_posts_navigation();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+			else :
 
-			endwhile;
+				get_template_part( 'template-parts/content', 'none' );
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+			endif;
+			?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
