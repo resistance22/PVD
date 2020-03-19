04e27699f6729400572b0af37a40447d0ea8a247
amin <aminforoutan7@gmail.com>
2020-03-19 15:11:14 +0330

    removing the tag:,category:.... of the_archive_title

--- functions.php
+++ functions.php
@@ -161,3 +161,17 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+add_filter( 'get_the_archive_title', function ($title) {    
+	if ( is_category() ) {    
+			$title = single_cat_title( '', false );    
+		} elseif ( is_tag() ) {    
+			$title = single_tag_title( '', false );    
+		} elseif ( is_author() ) {    
+			$title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+		} elseif ( is_tax() ) { //for custom post types
+			$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+		} elseif (is_post_type_archive()) {
+			$title = post_type_archive_title( '', false );
+		}
+	return $title;    
+});
