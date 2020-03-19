a360ee745f5347575293aeab5f0e64633463558e
amin <aminforoutan7@gmail.com>
2020-03-19 16:26:06 +0330

    creatign the content-page header

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<header class="page-header">
+		<h1>
+			<?php the_title(); ?>
+		</h1>
+	</header><!-- .page-header -->
 
 	<?php pvd_post_thumbnail(); ?>
 
